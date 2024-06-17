507219c9e65aae9d5155d6dfbc83d8fa20357eb5
ad <codemail.ad@gmail.com>
2024-06-17 11:57:28 +0200

    Check if maze has a valid path

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -1,5 +1,9 @@
 #include <Maze.h>
 #include <stdexcept>
+#include <array>
+#include <stack>
+#include <algorithm>
+#include <queue>
 
 Maze::Maze(const int rows, const int cols) : rows_(rows), cols_(cols) {
     start_ = {rows_ - 2, 1};
@@ -38,4 +42,36 @@ void Maze::setCellType(int row, int col, CellType& type) {
 
 bool Maze::isValidPosition(int row, int col) const {
     return (0 <= row && row <= rows_-1 && 0 <= col && col <= cols_-1);
+}
+
+bool Maze::hasPathFromStartToEnd() const {
+    std::queue<std::pair<int, int>> queue;
+    std::vector<std::vector<bool>> visited(rows_, std::vector<bool>(cols_, false));
+
+    queue.push(start_);
+    visited[start_.first][start_.second] = true;
+
+    while (!queue.empty()) {
+        auto [y, x] = queue.front();
+        queue.pop();
+
+        if (std::make_pair(y, x) == end_) {
+            return true;
+        }
+
+        static const std::array<std::pair<int, int>, 4> directions = {
+                {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+        };
+
+        for (const auto& [dy, dx] : directions) {
+            int ny = y + dy;
+            int nx = x + dx;
+
+            if (isValidPosition(ny, nx) && !visited[ny][nx] && maze_[ny][nx] != CellType::WALL) {
+                queue.emplace(ny, nx);
+                visited[ny][nx] = true;
+            }
+        }
+    }
+    return false;
 }
