0012a67b41b625a82aab4310a5a0281537320cb2
ad <codemail.ad@gmail.com>
2024-06-17 14:35:46 +0200

    Add heuristic function (Manhattan distance)

--- src/AStar.cpp
+++ src/AStar.cpp
@@ -2,3 +2,9 @@
 
 AStar::AStar(const Maze &maze) : maze_(maze) {}
 
+int AStar::heuristic(const std::pair<int, int> &a, const std::pair<int, int> &b) {
+    return std::abs(a.first - b.first) + std::abs(a.second - b.second);
+}
+
+
+
