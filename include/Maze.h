28897c489932299e8675fa70692e53fce1f606eb
ad <codemail.ad@gmail.com>
2024-06-17 11:16:21 +0200

    Add Maze header file

---
+++ include/Maze.h
@@ -0,0 +1,34 @@
+#ifndef MAZE_H
+#define MAZE_H
+
+#include <CellType.h>
+#include <vector>
+#include <utility>
+#include <random>
+
+class Maze {
+public:
+    Maze(int rows, int cols);
+    [[nodiscard]] int getWidth() const;
+    [[nodiscard]] int getHeight() const;
+    [[nodiscard]] std::pair<int, int> getStart() const;
+    [[nodiscard]] std::pair<int, int> getEnd() const;
+    [[nodiscard]] CellType getCellType(int row, int col) const;
+    void setCellType(int row, int col, CellType& type);
+    ~Maze() = default;
+
+private:
+    [[nodiscard]] bool isValidPosition(int row, int col) const;
+    [[nodiscard]] bool hasPathFromStartToEnd() const;
+    void generateRandomMaze();
+    void resetMaze();
+
+    int rows_;
+    int cols_;
+    std::vector<std::vector<CellType>> maze_;
+    std::pair<int, int> start_;
+    std::pair<int, int> end_;
+//    bool hasPathFromStartToEnd_;
+};
+
+#endif
---
+++ src/Maze.cpp
@@ -0,0 +1,10 @@
+#include <Maze.h>
+
+Maze::Maze(const int rows, const int cols) : rows_(rows), cols_(cols) {
+    start_ = {rows_ - 2, 1};
+    end_ = {1, cols_-2};
+    maze_= std::vector<std::vector<CellType>>(rows_, std::vector<CellType>(cols_, CellType::WALL));
+    generateRandomMaze();
+}
+
+
